dd5fe24a4bcb983d3147d6b26b14aa8a79426b4d
danjudchi <danieljude1992@gmail.com>
2024-11-08 05:07:52 +0000

    portfolio website

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple alert example
+function showAlert() {
+    alert("Hello! Thanks for visiting my portfolio.");
+}
+
+// Call the function on page load (just for demo)
+window.onload = showAlert;
